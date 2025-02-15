9dea0617a6232f78fd37d1efbf2ba34fd7b24c25
ViniBelo <vinialvesbelo@outlook.com>
2025-02-15 13:13:37 -0300

    Initial Commit

---
+++ Carangas/CarangasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarangasApp.swift
+//  Carangas
+//
+//  Created by Vinicius Alves Beló on 15/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CarangasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carangas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Carangas
+//
+//  Created by Vinicius Alves Beló on 15/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
